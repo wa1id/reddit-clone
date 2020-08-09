d63332b22597c3721406fb6d71b63546c49722a6
Wa1iD <wyachou95@gmail.com>
2020-08-09 16:16:39 +0200

    Add CommentRepository

---
+++ src/main/java/com/example/springredditclone/repository/CommentRepository.java
@@ -0,0 +1,13 @@
+package com.example.springredditclone.repository;
+
+import com.example.springredditclone.model.Comment;
+import com.example.springredditclone.model.Post;
+import com.example.springredditclone.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Long, Comment> {
+    List<Comment> findByPost(Post post);
+    List<Comment> findAllByUser(User user);
+}
