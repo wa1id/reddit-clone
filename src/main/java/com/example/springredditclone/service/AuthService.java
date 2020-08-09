cbaad8c3664aa9a914b22881fa387591bb92091d
Wa1iD <wyachou95@gmail.com>
2020-08-09 16:58:17 +0200

    Remove autowired (not using field injection). Replace with final and use constructor injection by using Lombok to create the constructor

--- src/main/java/com/example/springredditclone/service/AuthService.java
+++ src/main/java/com/example/springredditclone/service/AuthService.java
@@ -3,21 +3,22 @@ package com.example.springredditclone.service;
 import com.example.springredditclone.dto.RegisterRequest;
 import com.example.springredditclone.model.User;
 import com.example.springredditclone.repository.UserRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
 
 @Service
+@AllArgsConstructor
 public class AuthService {
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
 
-    @Autowired
-    private UserRepository userRepository;
-
+    @Transactional
     public void signup(RegisterRequest registerRequest) {
         User user = new User();
         user.setUsername(registerRequest.getUsername());
