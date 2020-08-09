708a3c447b26d07c87521dd7c34c3dea5353f078
Wa1iD <wyachou95@gmail.com>
2020-08-09 16:20:11 +0200

    Fix JpaRepository

--- src/main/java/com/example/springredditclone/repository/CommentRepository.java
+++ src/main/java/com/example/springredditclone/repository/CommentRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface CommentRepository extends JpaRepository<Long, Comment> {
+public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByPost(Post post);
     List<Comment> findAllByUser(User user);
 }
--- src/main/java/com/example/springredditclone/repository/SubredditRepository.java
+++ src/main/java/com/example/springredditclone/repository/SubredditRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface SubredditRepository extends JpaRepository<Long, Subreddit> {
+public interface SubredditRepository extends JpaRepository<Subreddit, Long> {
     Optional<Subreddit> findByName(String subredditName);
 }
