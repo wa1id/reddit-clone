8c76e9dfdbaa0ed2ee47cfda6c45e95255d3d017
wa1id <wa1id@users.noreply.github.com>
2020-08-09 18:07:51 +0200

    implement signup in AuthController

--- src/main/java/com/example/springredditclone/controller/AuthController.java
+++ src/main/java/com/example/springredditclone/controller/AuthController.java
@@ -1,6 +1,10 @@
 package com.example.springredditclone.controller;
 
 import com.example.springredditclone.dto.RegisterRequest;
+import com.example.springredditclone.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
+@AllArgsConstructor
 public class AuthController {
 
-    @PostMapping("/signup")
-    public void signup(@RequestBody RegisterRequest registerRequest) {
+    private final AuthService authService;
 
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest) {
+        authService.signup(registerRequest);
+        return new ResponseEntity<>("User Registration Successful", HttpStatus.OK);
     }
 }
