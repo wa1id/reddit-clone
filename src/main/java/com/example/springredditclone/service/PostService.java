e77bf278bc9d6903f34d5df1dc282021565e5917
wa1id <wa1id@users.noreply.github.com>
2020-08-10 21:38:16 +0200

    PostController logic

---
+++ src/main/java/com/example/springredditclone/service/PostService.java
@@ -0,0 +1,75 @@
+package com.example.springredditclone.service;
+
+import com.example.springredditclone.dto.PostRequest;
+import com.example.springredditclone.dto.PostResponse;
+import com.example.springredditclone.exceptions.PostNotFoundException;
+import com.example.springredditclone.exceptions.SubredditNotFoundException;
+import com.example.springredditclone.mapper.PostMapper;
+import com.example.springredditclone.model.Post;
+import com.example.springredditclone.model.Subreddit;
+import com.example.springredditclone.model.User;
+import com.example.springredditclone.repository.PostRepository;
+import com.example.springredditclone.repository.SubredditRepository;
+import com.example.springredditclone.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
+
+@Service
+@AllArgsConstructor
+@Transactional
+public class PostService {
+
+    private final SubredditRepository subredditRepository;
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+    private final AuthService authService;
+    private final PostMapper postMapper;
+
+    public void save(PostRequest postRequest) {
+        Subreddit subreddit = subredditRepository.findByName(postRequest.getSubredditName())
+                .orElseThrow(() -> new SubredditNotFoundException("Did not find subreddit: " + postRequest.getSubredditName()));
+
+        User currentUser = authService.getCurrentUser();
+
+        postRepository.save(postMapper.map(postRequest, subreddit, currentUser));
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostResponse> getAllPosts() {
+        return postRepository.findAll()
+                .stream()
+                .map(postMapper::mapToDto)
+                .collect(toList());
+    }
+
+    @Transactional(readOnly = true)
+    public PostResponse getPost(Long id) {
+        Post post = postRepository.findById(id)
+                .orElseThrow(() -> new PostNotFoundException(id.toString()));
+
+        return postMapper.mapToDto(post);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostResponse> getPostsBySubreddit(Long subredditId) {
+        Subreddit subreddit = subredditRepository.findById(subredditId)
+                .orElseThrow(() -> new SubredditNotFoundException("Subreddit not found with id: " + subredditId));
+        List<Post> posts = postRepository.findAllBySubreddit(subreddit);
+        return posts.stream().map(postMapper::mapToDto).collect(toList());
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostResponse> getPostsByUsername(String username) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+
+        return postRepository.findByUser(user).stream().map(postMapper::mapToDto).collect(toList());
+    }
+}
