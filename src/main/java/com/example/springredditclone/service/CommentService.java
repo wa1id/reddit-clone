f5d213cb3d6549660503aa70a021e7bfe020ebff
wa1id <wa1id@users.noreply.github.com>
2020-08-12 02:05:49 +0200

    Send notification mail to original poster

--- src/main/java/com/example/springredditclone/service/CommentService.java
+++ src/main/java/com/example/springredditclone/service/CommentService.java
@@ -4,10 +4,11 @@ import com.example.springredditclone.dto.CommentsDto;
 import com.example.springredditclone.exceptions.PostNotFoundException;
 import com.example.springredditclone.mapper.CommentMapper;
 import com.example.springredditclone.model.Comment;
+import com.example.springredditclone.model.NotificationEmail;
 import com.example.springredditclone.model.Post;
+import com.example.springredditclone.model.User;
 import com.example.springredditclone.repository.CommentRepository;
 import com.example.springredditclone.repository.PostRepository;
-import com.example.springredditclone.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,11 +16,13 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class CommentService {
 
+    //private static final String POST_URL = ""; // TODO: implement URL to the post in email
     private final PostRepository postRepository;
-    private final UserRepository userRepository;
     private final CommentRepository commentRepository;
     private final AuthService authService;
     private final CommentMapper commentMapper;
+    private final MailContentBuilder mailContentBuilder;
+    private final MailService mailService;
 
     public void save(CommentsDto commentsDto) {
         Post post = postRepository.findById(commentsDto.getPostId())
@@ -27,5 +30,12 @@ public class CommentService {
 
         Comment comment = commentMapper.map(commentsDto, post, authService.getCurrentUser());
         commentRepository.save(comment);
+
+        sendCommentNotificationToOriginalPoster(post.getUser(), comment.getUser());
+    }
+
+    private void sendCommentNotificationToOriginalPoster(User originalPoster, User commenter) {
+        String message = mailContentBuilder.build(commenter.getUsername() + " posted a comment on your post.");
+        mailService.sendMail(new NotificationEmail(commenter.getUsername() + " commented on your post", originalPoster.getEmail(), message));
     }
 }
