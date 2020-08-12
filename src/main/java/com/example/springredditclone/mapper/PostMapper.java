567f81e3a8555b3a9110277f4c4c32544e7f83be
wa1id <wa1id@users.noreply.github.com>
2020-08-12 20:34:41 +0200

    Modify PostMapper to get commentcount and the duration with Timeago library

--- src/main/java/com/example/springredditclone/mapper/PostMapper.java
+++ src/main/java/com/example/springredditclone/mapper/PostMapper.java
@@ -5,17 +5,32 @@ import com.example.springredditclone.dto.PostResponse;
 import com.example.springredditclone.model.Post;
 import com.example.springredditclone.model.Subreddit;
 import com.example.springredditclone.model.User;
+import com.example.springredditclone.repository.CommentRepository;
+import com.example.springredditclone.repository.VoteRepository;
+import com.example.springredditclone.service.AuthService;
+import com.github.marlonlom.utilities.timeago.TimeAgo;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(componentModel = "spring")
-public interface PostMapper {
+public abstract class PostMapper {
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Autowired
+    private VoteRepository voteRepository;
+
+    @Autowired
+    private AuthService authService;
 
     @Mapping(target = "createdDate", expression = "java(java.time.Instant.now())")
     @Mapping(target = "subreddit", source = "subreddit")
     @Mapping(target = "user", source = "user")
     @Mapping(target = "description", source = "postRequest.description")
-    Post map(PostRequest postRequest, Subreddit subreddit, User user);
+    @Mapping(target = "voteCount", constant = "0")
+    public abstract Post map(PostRequest postRequest, Subreddit subreddit, User user);
 
     @Mapping(target = "id", source = "postId")
     @Mapping(target = "postName", source = "postName")
@@ -23,5 +38,15 @@ public interface PostMapper {
     @Mapping(target = "url", source = "url")
     @Mapping(target = "subredditName", source = "subreddit.name")
     @Mapping(target = "username", source = "user.username")
-    PostResponse mapToDto(Post post);
+    @Mapping(target = "commentCount", expression = "java(commentCount(post))")
+    @Mapping(target = "duration", expression = "java(getDuration(post))")
+    public abstract PostResponse mapToDto(Post post);
+
+    Integer commentCount(Post post) {
+        return commentRepository.countByPost(post);
+    }
+
+    String getDuration(Post post) {
+        return TimeAgo.using(post.getCreatedDate().toEpochMilli());
+    }
 }
