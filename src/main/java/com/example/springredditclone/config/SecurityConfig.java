cb849f1d1702a835c9bdd67dd9214185dbf5a7ec
wa1id <wa1id@users.noreply.github.com>
2020-08-13 18:57:48 +0200

    Add Configuration annotation in SecurityConfig

--- src/main/java/com/example/springredditclone/config/SecurityConfig.java
+++ src/main/java/com/example/springredditclone/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.example.springredditclone.security.JwtAuthenticationFilter;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -15,6 +16,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Configuration
 @EnableWebSecurity
 @AllArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
